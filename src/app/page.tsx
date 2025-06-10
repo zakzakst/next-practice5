32a4cd666fa019c8324d260f730009264c6dad13
zakzakst <e1433104@gmai.com>
2025-06-10 22:12:31 +0900

    修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,9 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button ref={ref}>ボタン</Button>
+      <Button ref={ref} variant="text">
+        ボタン
+      </Button>
     </div>
   );
 }
